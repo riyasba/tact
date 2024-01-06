e86b38e2c2150fefae23f9e2096240d956d93496
anasnaji25 <anasnaji4848@gmail.com>
2024-01-06 15:37:53 +0530

    ch

--- lib/services/base_url/base_url.dart
+++ lib/services/base_url/base_url.dart
@@ -1,15 +1,15 @@
 abstract class BaseApiServices {
   final String storesubcatogoryURL =
-      'https://bci.batechnology.in/api/store_sub_category';
+      'https://tact.batechnology.in/api/store_sub_category';
   final String getsubcategoryURL =
-      "https://bci.batechnology.in/api/get_sub_category";
-  final String getcategoryURL = "https://bci.batechnology.in/api/get_category";
-  final String getactivitysURL = "https://bci.batechnology.in/api/get_activity";
+      "https://tact.batechnology.in/api/get_sub_category";
+  final String getcategoryURL = "https://tact.batechnology.in/api/get_category";
+  final String getactivitysURL = "https://tact.batechnology.in/api/get_activity";
   final String storeactivityURL =
-      'https://bci.batechnology.in/api/store_activity';
+      'https://tact.batechnology.in/api/store_activity';
   final String deleteactivityURL =
-      "https://bci.batechnology.in/api/delete_activity";
+      "https://tact.batechnology.in/api/delete_activity";
 
   final String deletetempsubcatogory =
-      "https://bci.batechnology.in/api/delete_subcategory";
+      "https://tact.batechnology.in/api/delete_subcategory";
 }
